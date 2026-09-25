3f6465d1e99f55f8ca488767ef435b134ec688d0
mnfilipe <chipangam46@gmail.com>
2026-09-25 03:57:01 +0200

    Add files via upload

--- CPP/matola_llama_jni.cpp
+++ CPP/matola_llama_jni.cpp
@@ -410,7 +410,7 @@ Java_com_example_matolaia_apk_MatolaLlama_nativeCompletion(
     llama_sampler_chain_params sparams = llama_sampler_chain_default_params();
     llama_sampler *smpl = llama_sampler_chain_init(sparams);
 
-    llama_sampler_chain_add(smpl, llama_sampler_init_penalties(llama_vocab_n_tokens(mc->vocab), 64, 1.05f, 0.0f, 0.0f));
+    llama_sampler_chain_add(smpl, llama_sampler_init_penalties(64, 1.05f, 0.0f, 0.0f));
     llama_sampler_chain_add(smpl, llama_sampler_init_top_k(40));
     llama_sampler_chain_add(smpl, llama_sampler_init_top_p(0.95f, 1));
     llama_sampler_chain_add(smpl, llama_sampler_init_min_p(0.05f, 1));
